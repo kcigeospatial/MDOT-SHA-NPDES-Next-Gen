c7db332a5d759ed0a86a62b24a30280bb5b6dd6f
Elliott Plack <talllguy@users.noreply.github.com>
2018-04-11 16:47:11 -0400

    update script comment formatting
    moved commits from the first commit (8267d3168139398021f6ac602fd304bbdc8f48a7) to the script itself and added some spacing and formatting as I interpreted it

--- Export_Attachments.py
+++ Export_Attachments.py
@@ -1,31 +1,40 @@
+#############################################
+# Export Attachments python script
+# By: @dobrochtaKCI
+#############################################
+
+## Summary
+#  There are no geoprocessing tools that allow users to export and save all
+#      attachments locally. The instructions provided below describe a possible solution.
+#  The following script iterates through the entire attachment table of a single
+#      feature class and copies all of the attachments (saved as BLOBs, or binary large
+#      objects) to file.
+
 import arcpy
 from arcpy import da
 import os
 
-#base code and instructions for use come from https://support.esri.com/en/technical-article/000011912
-#This script will need to be added to an ArcGIS toolboxx to work properly
-#it requires the first parameter to be a table and the second parameter to be a folder
+# Base code and instructions for use come from https://support.esri.com/en/technical-article/000011912
+# This script will need to be added to an ArcGIS toolbox to work properly
+#     it requires the first parameter to be a table and the second parameter to be a folder
 
 
-#Input Attachment Table 
+# Input Attachment Table 
 inTable = arcpy.GetParameterAsText(0)
-#Location folder of output attachments
+# Location folder of output attachments
 fileLocation = arcpy.GetParameterAsText(1)
 
 
-#Add a meaningful name to the attachments
+## Add a meaningful name to the attachments
+#  1. Add a new field to Attachement table, in this scenario I called the field 'Label'
+#  2. join related table and or features back to the attachment table (Start with attachments and move backwards)
+#      especially if using a 1:M feature to attachment relationship
+#  3. field calculate a label used for each picture using a unique ID or a combination of fields, which will provide the
+#      primary label for each picture
 
-#Add a new field to Attachement table, in this scenario I called the field 'Label'
+# The fields `Label`, `ATTACHMENTID`, and `ATT_NAME` are used to build the final picture filename
 
-#join related table and or features back to the attachment table(Start with attachments and move backwards)
-#especially if using a 1:M feature to attachment relationship
-
-#field calculate a label used for each picture using a unique ID or a combination of fields, which will provide the
-#primary label for each picture
-
-#fields Label, ATTACHMENTID, and ATT_NAME are used to build the final picture filename
-
-#fields DATA, ATTACHMENTID, and ATT_NAME are native to Attachment tables in ArcGIS
+# fields DATA, ATTACHMENTID, and ATT_NAME are native to Attachment tables in ArcGIS
 with da.SearchCursor(inTable, ['DATA', 'Label','ATTACHMENTID', 'ATT_NAME']) as cursor:
     for item in cursor:
         attachment = item[0]
